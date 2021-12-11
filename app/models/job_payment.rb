219c312e02de6a9f3d5a2226bd0308b858b9c88c
Gareth Phillips <gareth326771@hotmail.com>
2021-12-11 17:24:15 +0000

    g

--- app/models/job_payment.rb
+++ app/models/job_payment.rb
@@ -1,4 +1,8 @@
 class JobPayment < ApplicationRecord
   attr_accessor :initiating_party
   attr_accessor :tradesman_email
+  attr_accessor :customer_email
+  attr_accessor :job_cost
+  attr_accessor :short_description
+
 end
