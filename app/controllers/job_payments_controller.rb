6864c7995d21b2c40fb005f97f85c7ef76416eb9
Gareth Phillips <gareth326771@hotmail.com>
2021-12-13 21:23:38 +0000

    f

--- app/controllers/job_payments_controller.rb
+++ app/controllers/job_payments_controller.rb
@@ -3,4 +3,18 @@ class JobPaymentsController < ApplicationController
   def new
     @job_payement = JobPayment.new
   end
+
+  def create
+
+    respond_to do |format|
+      format.js
+    end
+    # @job_payment = JobPayment.create(job_payment_params)
+    # if @job_payment.save
+    #   render head :ok
+    # else
+    #   flash[:notice] = "Could not start payment. Please check form details."
+    #   redirect_to new_job_payments_path
+    # end
+  end
 end
