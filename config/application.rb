532266cc624871b20fe75dcc0580bc783c067ccf
Gareth Phillips <gareth326771@hotmail.com>
2022-03-04 18:34:27 +0000

    f

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,14 @@ module Truepenny
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.serve_static_assets = true
+    config.assets.compile = true
+    config.assets.digest = true
+    config.assets.version = '1.0'
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
