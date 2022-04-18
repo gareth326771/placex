29fdd2ff416eeb7f92e6cc7ea7231c56556305d0
Gareth Perilli <garethperilli@gmail.com>
2022-04-18 15:16:26 +0900

    sharing OG cards setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
 
+  # OG card meta data setting
+  # Needs to be set on Heroku with: heroku config:set DOMAIN=www.my_product.com
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
     def configure_permitted_parameters
